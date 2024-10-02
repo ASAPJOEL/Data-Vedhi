837ba1ec25e68d4e2826b0e6fe6b61d49ec9c09a
ASAPJOEL <anirudhsonu.2005@gmail.com>
2024-10-02 17:12:06 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,19 @@
+# first Create a list
+my_list = []
+
+# Append elements to the list
+def to_append_list(element):
+    my_list.append(element)
+    print(f"Appended {element} to the list. Current list: {my_list}")
+
+# Delete element from the list by value
+def to_delete_from_list(element):
+    if element in my_list:
+        my_list.remove(element)
+        print(f"Deleted {element} from the list. Current list: {my_list}")
+    else:
+        print(f"{element} not found in the list.")
+
+# usage
+append_to_list(10)
+delete_from_list(20)
